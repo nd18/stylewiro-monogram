4be27356b186ee56912f71a73c413a9dbcc03262
nd18 <neel.malaviya18@gmail.com>
2025-01-14 00:57:30 +0530

    Cart Drawer Add Edit Functionality Done

--- assets/embroidered.js
+++ assets/embroidered.js
@@ -29,7 +29,6 @@ function toggleAccordion(element) {
       resetHiddenInputs();
     } else {
       hiddenColorProductTime.removeAttribute("disabled");
-      productUnikTime.removeAttribute("disabled");
     }
   }
 
@@ -90,7 +89,6 @@ function resetHiddenInputs() {
 
   // Time Inputs
   hiddenColorProductTime.setAttribute("disabled", "disabled");
-  productUnikTime.setAttribute("disabled", "disabled");
 }
 
 // Event Listener for Embroidered Name Input
@@ -152,8 +150,6 @@ const cartEmbroideredNameInput = document.querySelectorAll(".drawer__contents .c
 const cartEmbroideredColorInputs = document.querySelectorAll(".drawer__contents .product-color input[type='radio']");
 const cartEmbroideredFontInputs = document.querySelectorAll(".drawer__contents .product-font-option input[type='radio']");
 
-const cartUpdateButton = document.querySelectorAll(".drawer__contents .update_btn");
-
 // Helper function to check changes and toggle update button
 function toggleUpdateButton(prentDOM) {
   const updateBtn = prentDOM.querySelector(".update_btn");
@@ -202,70 +198,114 @@ function toggleUpdateButton(prentDOM) {
 }
 
 // Name: Trigger change on input
-cartEmbroideredNameInput.forEach((input) => {
-  input.addEventListener("input", function (event) {
-    const prentDOM = event.currentTarget.closest("#embroidered_cart_option");
+document.addEventListener("input", function (event) {
+  if (event.target.matches("input.cart_embroidered_name")) {
+    const prentDOM = event.target.closest("#embroidered_cart_option");
     toggleUpdateButton(prentDOM);
-  });
+  }
 });
 
 // Color: Trigger change on radio button selection
-cartEmbroideredColorInputs.forEach((radioInput) => {
-  radioInput.addEventListener("change", function (event) {
-    const prentDOM = event.currentTarget.closest("#embroidered_cart_option");
+document.addEventListener("change", function (event) {
+  if (event.target.matches(".product-color input") || event.target.matches(".product-font-option input")) {
+    const prentDOM = event.target.closest("#embroidered_cart_option");
     toggleUpdateButton(prentDOM);
-  });
-});
-
-// Fonts: Trigger change on radio button selection
-cartEmbroideredFontInputs.forEach((input) => {
-  input.addEventListener("change", function (event) {
-    const prentDOM = event.currentTarget.closest("#embroidered_cart_option");
-    toggleUpdateButton(prentDOM);
-  });
+  }
 });
 
 // Update Data
-cartUpdateButton.forEach((updatebtn) => {
-  updatebtn.addEventListener("click", function (event) {
-    const prentDOM = event.currentTarget.closest("#embroidered_cart_option");
-    const item_key = prentDOM.getAttribute("data-line-key");
+function cartUpdateButtonEvent(event) {
+  const prentDOM = event.currentTarget.closest("#embroidered_cart_option");
+  const data_line_key = prentDOM.getAttribute("data-line-key");
+  const data_embroidered_name = prentDOM.getAttribute("data-Embroidered-Name");
+  const data_embroidered_color = prentDOM.getAttribute("data-Embroidered-Color");
+  const data_embroidered_color_variant_id = prentDOM.getAttribute("data-Embroidered-Color-Variant-ID");
+  const embroideredUnikTime = prentDOM.getAttribute("data-Embroidered-Unik-Time");
+  const data_embroidered_font = prentDOM.getAttribute("data-Embroidered-Font");
 
-    const newProperties = [];
+  // Initialize an object to store the properties
+  const newProperties = {};
 
-    // Check name input
-    const embroideredName = prentDOM.querySelector("input.cart_embroidered_name");
-    const embroideredNameVal = embroideredName.value.trim();
-    const embroideredNameDeVal = embroideredName.parentElement.getAttribute('data-default-name');
-    if (embroideredNameVal !== embroideredNameDeVal) {
-      newProperties.push({ 'Embroidered Name': embroideredNameVal });
+  // Check name input
+  const embroideredName = prentDOM.querySelector("input.cart_embroidered_name");
+  const embroideredNameVal = embroideredName.value.trim();
+  if (embroideredNameVal !== data_embroidered_name) {
+    newProperties['Embroidered Name'] = embroideredNameVal;
+  } else {
+    newProperties['Embroidered Name'] = data_embroidered_name;
+  }
+
+  // Check color input
+  const embroideredColor = prentDOM.querySelector(".product-color input:checked");
+  const embroideredColorVal = embroideredColor ? embroideredColor.value.trim() : "";
+  const embroideredColorVarID = embroideredColor ? embroideredColor.parentElement.getAttribute('data-var-id') : "";
+  if (embroideredColorVal !== data_embroidered_color) {
+    newProperties['Embroidered Color'] = embroideredColorVal;
+    newProperties['Embroidered Color Variant ID'] = embroideredColorVarID;
+    newProperties['Embroidered Unik Time'] = embroideredUnikTime;
+  } else {
+    newProperties['Embroidered Color'] = data_embroidered_color;
+    newProperties['Embroidered Color Variant ID'] = data_embroidered_color_variant_id;
+    newProperties['Embroidered Unik Time'] = embroideredUnikTime;
+  }
+
+  // Check font input
+  const embroideredFont = prentDOM.querySelector(".product-font-option input:checked");
+  const embroideredFontVal = embroideredFont ? embroideredFont.value.trim() : "";
+  if (embroideredFontVal !== data_embroidered_font) {
+    newProperties['Embroidered Font'] = embroideredFontVal;
+  } else {
+    newProperties['Embroidered Font'] = data_embroidered_font;
+  }
+
+  const timePropertie = { 'time': parseInt(embroideredUnikTime) }
+  let cartUpdate = true;
+  if (embroideredColorVarID !== data_embroidered_color_variant_id) {
+    cartUpdate = false;
+  }
+
+  updateProductproperties(data_line_key, newProperties, cartUpdate, function () {
+    if (embroideredColorVarID !== data_embroidered_color_variant_id) {
+      updateEmbroideredProduct(parseInt(embroideredUnikTime), false)
+      setTimeout(function () {
+        addToCart_Ajax(embroideredColorVarID, timePropertie, 0);
+      }, 1000);
     }
-
-    // Check color input
-    const embroideredColor = prentDOM.querySelector(".product-color input:checked");
-    const embroideredColorVal = embroideredColor ? embroideredColor.value.trim() : "";
-    const embroideredColorDeVal = embroideredColor ? embroideredColor.parentElement.getAttribute('data-default-color') : "";
-    const embroideredColorVarID = embroideredColor ? embroideredColor.parentElement.getAttribute('data-var-id') : "";
-    const embroideredUnikTime = prentDOM.getAttribute('data-embroidered_unik_time');
-    if (embroideredColorVal !== embroideredColorDeVal) {
-      newProperties.push({ 'Embroidered Color': embroideredColorVal });
-      newProperties.push({ 'Embroidered Color Variant ID': embroideredColorVarID });
-      newProperties.push({ 'Embroidered Unik Time': embroideredUnikTime });
-    }
-
-    // Check font input
-    const embroideredFont = prentDOM.querySelector(".product-font-option input:checked");
-    const embroideredFontVal = embroideredFont ? embroideredFont.value.trim() : "";
-    const embroideredFontDeVal = embroideredFont ? embroideredFont.parentElement.getAttribute('data-default-font') : "";
-    if (embroideredFontVal !== embroideredFontDeVal) {
-      newProperties.push({ 'Embroidered Font': embroideredFontVal });
-    }
-
-    console.log(newProperties);
-    // updateEmbroideredProduct(embroideredUnikTime, false, true)
-
   });
-});
+};
+
+// Add New Embroidered Button
+function cartAddButtonEvent(event) {
+  const prentDOM = event.currentTarget.closest("#embroidered_cart_option");
+  const data_line_key = prentDOM.getAttribute("data-line-key");
+
+  // Initialize an object to store the properties
+  const newProperties = {};
+
+  // Check name input
+  const embroideredName = prentDOM.querySelector("input.cart_embroidered_name");
+  const embroideredNameVal = embroideredName.value.trim();
+  newProperties['Embroidered Name'] = embroideredNameVal;
+
+  // Check color input
+  const embroideredColor = prentDOM.querySelector(".product-color input:checked");
+  const embroideredColorVal = embroideredColor ? embroideredColor.value.trim() : "";
+  const embroideredColorVarID = embroideredColor ? embroideredColor.parentElement.getAttribute('data-var-id') : "";
+  const embroideredUnikTime = prentDOM.getAttribute('data-Embroidered-Unik-Time');
+  newProperties['Embroidered Color'] = embroideredColorVal;
+  newProperties['Embroidered Color Variant ID'] = embroideredColorVarID;
+  newProperties['Embroidered Unik Time'] = embroideredUnikTime;
+
+  // Check font input
+  const embroideredFont = prentDOM.querySelector(".product-font-option input:checked");
+  const embroideredFontVal = embroideredFont ? embroideredFont.value.trim() : "";
+  newProperties['Embroidered Font'] = embroideredFontVal;
+
+  const timePropertie = { 'time': parseInt(embroideredUnikTime) }
+  updateProductproperties(data_line_key, newProperties, false, function () {
+    addToCart_Ajax(embroideredColorVarID, timePropertie, 0);
+  });
+}
 
 // Toggles the accordion visibility and resets associated inputs.
 function toggleCartAccordion(event, element, unikTime) {
@@ -277,7 +317,6 @@ function toggleCartAccordion(event, element, unikTime) {
     checkbox.checked = !checkbox.checked;
   }
   if (checkbox.checked) {
-    console.log("Check")
     accordion_content.classList.remove("hidden");
   } else {
     accordion_content.classList.add("hidden");
@@ -311,15 +350,15 @@ function updateEmbroideredProduct(unikTime, MainProductReset) {
               updateData[item.key] = 0;
               break;
             }
-            if (MainProductReset && key === 'Embroidered Unik Time' && intValue === unikTime) {
+            if (MainProductReset && key === 'Embroidered Unik Time' && intValue === unikTime && Object.keys(item.properties).length > 1) {
               const main_product_key = item.key;
-              const newProperties = { '_properties': '' };
-              updateProductproperties(main_product_key, newProperties)
+              const newProperties = { 'Embroidered Unik Time': unikTime };
+              updateProductproperties(main_product_key, newProperties, false, 0)
             }
           }
         }
       });
-      if (updateData != '') {
+      if (updateData && Object.keys(updateData).length > 0) {
         var cartDrawer = document.querySelector('cart-drawer');
         setTimeout(function () {
           fetch('/cart/update.js?cart_data_update', {
@@ -339,7 +378,6 @@ function updateEmbroideredProduct(unikTime, MainProductReset) {
               return response.json();
             })
             .then(cart => {
-              console.log('Embroidered Color Product Removed');
               getSectionsToRenderDrawer().forEach((section) => {
                 const elementToReplace = document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
                 elementToReplace.innerHTML = cartDrawer.getSectionInnerHTML(
@@ -351,7 +389,7 @@ function updateEmbroideredProduct(unikTime, MainProductReset) {
             .catch(error => {
               console.error('Update Data Error:', error);
             });
-        }, 500);
+        }, 1000);
       }
     })
     .catch(error => {
@@ -359,7 +397,8 @@ function updateEmbroideredProduct(unikTime, MainProductReset) {
     });
 }
 
-function updateProductproperties(lineItemKey, newProperties) {
+function updateProductproperties(lineItemKey, newProperties, cartUpdate, callback) {
+  var cartDrawer = document.querySelector('cart-drawer');
   fetch('/cart/change.js', {
     method: 'POST',
     headers: {
@@ -368,11 +407,23 @@ function updateProductproperties(lineItemKey, newProperties) {
     body: JSON.stringify({
       id: lineItemKey,
       properties: newProperties,
+      sections: getSectionsToRenderDrawer().map((section) => section.section),
     }),
   })
     .then((response) => response.json())
     .then((cart) => {
-      console.log('Update Product Properties');
+      if (cartUpdate) {
+        getSectionsToRenderDrawer().forEach((section) => {
+          const elementToReplace = document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
+          elementToReplace.innerHTML = cartDrawer.getSectionInnerHTML(
+            cart.sections[section.section],
+            section.selector
+          );
+        });
+      }
+      if (callback && callback !== 0 && typeof callback === "function") {
+        callback();
+      }
     })
     .catch((error) => {
       console.error('Error updating line item properties:', error);
@@ -394,31 +445,52 @@ function getSectionsToRenderDrawer() {
   ];
 }
 
-// Add to cart with callback
-function addToCartAjax(varId) {
-  const data = {
-    items: [
-      {
-        id: varId,
-        quantity: 1,
-      },
-    ],
+// Add To cart with callBack()
+function addToCart_Ajax(varID, properties_data, callback) {
+  var cartDrawer = document.querySelector('cart-drawer');
+  const data = [
+    {
+      id: varID,
+      quantity: 1
+    }
+  ];
+  if (properties_data && Object.keys(properties_data).length > 0) {
+    data[0].properties = properties_data;
+  }
+
+  // Prepare the request payload
+  const requestBody = {
+    items: data,
+    sections: getSectionsToRenderDrawer().map((section) => section.section),
   };
 
-  fetch('/cart/add.js', {
-    method: 'POST',
+  // Make an AJAX POST request using Fetch API
+  fetch("/cart/add.js", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(requestBody),
   })
     .then((response) => {
       if (!response.ok) {
-        throw new Error(`Error adding data to cart: ${response.statusText}`);
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
       }
       return response.json();
     })
+    .then((cart) => {
+      getSectionsToRenderDrawer().forEach((section) => {
+        const elementToReplace = document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
+        elementToReplace.innerHTML = cartDrawer.getSectionInnerHTML(
+          cart.sections[section.section],
+          section.selector
+        );
+      });
+      if (callback && callback !== 0 && typeof callback === "function") {
+        callback();
+      }
+    })
     .catch((error) => {
-      console.error('Error adding data to cart:', error);
+      console.error("Error adding data to cart:", error);
     });
 }
